018fadfe3566c6079aef4cdc7a939296c9ae2e6d
Raiden47 <tacotag@libero.it>
2022-11-14 21:02:30 +0100

    script commit

--- commit.c
+++ commit.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <sys/exec.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 int main (int argc, char *argv[]){
 
-  execlp("git", "git", "add", "--all");
+  execlp("ls", "ls", "-l", NULL);
+  perror("errore 0");
+  execlp("git", "git", "add", "--all", NULL);
   perror("errore 1");
-  execlp("git", "git", "commit", "-m", argv[1]);
+  execlp("git", "git", "commit", "-m", argv[1], NULL);
   perror("errore 2");
-  execlp("git", "git", "push", "--set-upstream", "origin", "main");
+  execlp("git", "git", "push", "--set-upstream", "origin", "main", NULL);
   perror("errore 3");
 
   return 0;
