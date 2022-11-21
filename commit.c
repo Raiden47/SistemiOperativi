4cf86c69062a08fef053d341b0dceca6ccd1d65d
Raiden47 <tacotag@libero.it>
2022-11-21 23:57:01 +0100

    UpdateCommit

--- commit.c
+++ commit.c
@@ -5,15 +5,42 @@
 
 int main (int argc, char *argv[]){
 
-  printf("Messaggio da stampare inserire con il commit >> %s\n\n\n", argv[1]);
-  execlp("ls", "ls", "-l", NULL);
-  perror("errore 0");
-  execlp("git", "git", "add", "--all", NULL);
-  perror("errore 1");
-  execlp("git", "git", "commit", "-m", argv[1], NULL);
-  perror("errore 2");
-  execlp("git", "git", "push", "--set-upstream", "origin", "main", NULL);
-  perror("errore 3");
+  int pid;
+  int st;
+
+  for (int i = 0 ; i < 3 ; i++){
+
+    //Create a new process
+    pid = fork();
+    if (pid == 0 && i == 0){
+      //git add --all
+      execlp("git", "git", "add", "--all", NULL);
+      perror("errore 1");
+      _exit(0);
+    } else if (pid == 0 && i == 1){
+      //git commit -m "mesaggio"
+      execlp("git", "git", "commit", "-m", argv[1], NULL);
+      perror("errore 2");
+      _exit(0);
+    } else if (pid == 0 && i == 2){
+      //git push --set-upstrea origin main
+      execlp("git", "git", "push", "--set-upstream", "origin", "main", NULL);
+      perror("errore 3");
+      _exit(0);
+    }
+
+  }
+    if (pid > 0){
+      for (int i = 0 ; i < 3 ; i++){
+        wait(&st);
+        printf("Il processo è terminato con stato : %d\n", st);
+      }
+    }
+
+
+
+
+
 
   return 0;
 
