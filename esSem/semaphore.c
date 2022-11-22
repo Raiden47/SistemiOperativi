5acb66c0c3b44cb9e416e3775074494a017231bb
Raiden47 <tacotag@libero.it>
2022-11-22 15:14:17 +0100

    update semaphore

--- esSem/semaphore.c
+++ esSem/semaphore.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
-#include <unistd.h>
 
 int wait_sem (int id_sem, int num_sem){
   int err;
   struct sembuf sem_buf;
 
+  sem_buf.sem_num = num_sem;
+  sem_buf.sem_flg = 0;
+  sem_buf.sem_op = -1;
 }
